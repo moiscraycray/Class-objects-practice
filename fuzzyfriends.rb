a89a9b5b4b961a45d26ad34d1e02cfc689385705
Olivia Mo <oliviatrademark@gmail.com>
2017-09-02 13:45:24 +1000

    added validation test

--- fuzzyfriends.rb
+++ fuzzyfriends.rb
@@ -1,53 +1,40 @@
 # This app is for practicing Ruby classes and objects.
 
-class Bird
-  def talk(name)
-    puts "#{name} says Chirp! Chirp!"
-  end
-
-  def move(name, destination)
-    puts "#{name} flies to the #{destination}."
-  end
-end
-
 class Dog
-  def name=(new_value)
-    @name = new_value
+
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    @name = value
   end
 
-  def name
-    @name
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+    @age = value
   end
 
-  def age=(new_value)
-    @age = new_value
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
   end
 
-  def age
-    @age
+  def talk
+    puts "#{name} says Bark!"
   end
 
   def report_age
-    puts "#{name} is #{@age} years old."
+    puts "#{@name} is #{@age} years old."
   end
 
 end
 
-class Cat
-  def talk(name)
-    puts "#{name} says Meow!"
-  end
-
-  def move(name, destination)
-    puts "#{name} runs to the #{destination}."
-  end
-end
-
-fido = Dog.new
-fido.name = "Fido"
-fido.age = 2
-rex = Dog.new
-rex.name = "Rex"
-rex.age = 3
-fido.report_age
-rex.report_age
+dog = Dog.new
+dog.name = "Daisy"
+dog.age = 3
+dog.report_age
+dog.talk
+dog.move("bed")
