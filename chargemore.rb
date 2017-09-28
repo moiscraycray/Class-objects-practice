3f553752ced14c1f090f6016cfb84345dfbb4edd
Olivia Mo <oliviatrademark@gmail.com>
2017-09-28 22:27:07 +1000

    added initialize method

--- chargemore.rb
+++ chargemore.rb
@@ -11,15 +11,6 @@ class Employee
   end
   # end validation
 
-  # def initialize(name = "Anonymous", salary = 0.0, hourly_wage = 0.0)
-  #   self.name = name # name = is the same as calling name=
-  #   self.salary = salary # same principle as above. self points to the object/instance (amy) itself
-  #   # we have to use 'self' because there'll be an error if we do amy.name = name
-  #   # also, amy (instance) is not in scope so we use 'self' to refer to the class itself
-  #   # Ruby definitely knows 'self.name=name' is a call to the 'name=' method. This enables validation
-  #   self.hourly_wage = hourly_wage
-  # end
-
   def print_name
     puts "Name: #{self.name}" # self.name is the same as name; it works if 'self' is omitted
   end
@@ -28,7 +19,15 @@ end
 
 class SalariedEmployee < Employee
 
-  attr_reader :salary
+  attr_reader :name, :salary
+
+  def initialize(name = "Anonymous", salary = 0.0) # Here, we are making parameters optional by providing defaults
+    self.name = name # name = is the same as calling name=
+    self.salary = salary # same principle as above. self points to the object/instance (amy) itself
+    # we have to use 'self' because there'll be an error if we do amy.name = name
+    # also, amy (instance) is not in scope so we use 'self' to refer to the class itself
+    # Ruby definitely knows 'self.name=name' is a call to the 'name=' method. This enables validation
+  end
 
   # validation: the following ensures valid arguments are given
   def salary=(salary) #setter
@@ -48,6 +47,13 @@ class SalariedEmployee < Employee
 end
 
 class HourlyEmployee < Employee
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    self.name = name
+    self.hourly_wage = hourly_wage
+    @hours_per_week = hours_per_week # here we used an instance variable instead of 'self'. It does the same thing since 'self' is optional provided the '@' is used
+  end
+  # This is the same as the previous initialize method
 
   attr_reader :hourly_wage, :hours_per_week
 
@@ -74,13 +80,8 @@ class HourlyEmployee < Employee
 
 end
 
-amy = SalariedEmployee.new #Try invalid arguments to test the methods name= and salary=
-amy.name = "Amy Blake"
-amy.salary = 50_000
+amy = SalariedEmployee.new("Amy Blake", 50_000) #Try invalid arguments to test the methods name= and salary=
 amy.print_pay_stub
 
-olivia = HourlyEmployee.new
-olivia.name = "Olivia Mo"
-olivia.hourly_wage = 23
-olivia.hours_per_week = 50
+olivia = HourlyEmployee.new("Olivia Mo", 23, 50)
 olivia.print_pay_stub
