ed2df2b9ee13db6754b674c9f922db51141afb2a
Olivia Mo <oliviatrademark@gmail.com>
2017-09-28 22:00:49 +1000

    updated comments, created SalariedEmployees subclass

--- chargemore.rb
+++ chargemore.rb
@@ -2,13 +2,33 @@ class Employee
 
   attr_reader :name, :salary #getter
 
+  # validation: the following ensures valid arguments are given
   def name=(name) #setter
     if name == ""
       raise "Name can't be blank!"
     end
     @name = name
   end
+  # end validation
 
+  def initialize(name = "Anonymous", salary = 0.0)
+    self.name = name # name = is the same as calling name=
+    self.salary = salary # same principle as above. self points to the object/instance (amy) itself
+    # we have to use 'self' because there'll be an error if we do amy.name = name
+    # also, amy (instance) is not in scope so we use 'self' to refer to the class itself
+    # Ruby definitely knows 'self.name=name' is a call to the 'name=' method. This enables validation
+  end
+
+  def print_name
+    puts "Name: #{self.name}" # self.name is the same as name; it works if 'self' is omitted
+  end
+
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  # validation: the following ensures valid arguments are given
   def salary=(salary) #setter
     if salary < 0
       raise "A salary of #{salary} isn't valid!"
@@ -16,21 +36,16 @@ class Employee
     @salary = salary
   end
 
-  def initialize(name = "Anonymous", salary = 0.0)
-    self.name = name # name = is the same as calling name=
-    self.salary = salary # same principle as above. self points to the object/instance (amy) itself
-    # have to use 'self' because there'll be an error if we do amy.name = name
-    # amy is not in scope
-    # Ruby definitely knows 'self.name=name' is a call to the 'name=' method
-  end
-
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = format("%.2f", @salary / 365.0 * 14)
-    puts "Pay This Period: $#{pay_for_period}"
+    print_name # This calls print_name method inherited from superclass Employee
+    pay_for_period = (self.salary / 365.0 * 14)
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay This Period: $#{formatted_pay}"
   end
 
 end
 
-amy = Employee.new("Amy Blake", 50_000) #Try invalid arguments to test the methods name= and salary=
+amy = SalariedEmployee.new #Try invalid arguments to test the methods name= and salary=
+amy.name = "Amy Blake"
+amy.salary = 50_000
 amy.print_pay_stub
